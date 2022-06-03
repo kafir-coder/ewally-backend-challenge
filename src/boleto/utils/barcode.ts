ea3a4083bef3b7b8d4f6ab869bbfe27bee0307ff
Caio Tony <caiotony16@gmail.com>
2022-06-03 16:31:44 +0100

    feat: add barcode utilities

---
+++ src/boleto/utils/barcode.ts
@@ -0,0 +1,17 @@
+export const getBankBarcode = (dline: string) =>
+  dline.replace(
+    /^(\d{4})(\d{5})\d{1}(\d{10})\d{1}(\d{10})\d{1}(\d{15})$/,
+    '$1$5$2$3$4',
+  );
+
+export const getAgreementBarcode = (dline: string) => {
+  let barcode = '';
+
+  for (let index = 0; index < 4; index += 1) {
+    const start = 11 * index + index;
+    const end = 11 * (index + 1) + index;
+
+    barcode += dline.substring(start, end);
+  }
+  return barcode;
+};
